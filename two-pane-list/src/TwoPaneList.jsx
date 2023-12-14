d0664d25e505adaa6a8e85ec05946561f59fd154
Alvin Zhao <14051545+yzAlvin@users.noreply.github.com>
2023-12-15 01:45:31 +1100

    Show content on click
    Leaving it ugly for now!

--- two-pane-list/src/TwoPaneList.jsx
+++ two-pane-list/src/TwoPaneList.jsx
@@ -1,8 +1,28 @@
+import { useState } from 'react'
+
 export const TwoPaneList = ({ albums }) => {
-  return <div className="tile is-ancestor">
+  const [selectedAlbum, setSelectedAlbum] = useState()
+
+  return <div className="tile">
     <div className="tile is-vertical has-background-primary rounded">
-      {albums.map(album => <p>{album.title}</p>)}
+      {albums.map(album => <Title album={album} setSelectedAlbum={setSelectedAlbum} />)}
     </div>
-    <div className="tile is-vertical has-background-info"></div>
+    <div className="tile is-vertical has-background-info">
+      {selectedAlbum ? 
+        <Content content={selectedAlbum.content} /> : 
+        <p>Click a Title!</p>
+      }
+    </div>
   </div>
 }
+
+const Title = ({album, setSelectedAlbum}) => {
+  const setAlbum = () => {
+    setSelectedAlbum(album)
+  }
+
+  return <button type="button" onClick={setAlbum}>{album.title}</button>
+}
+
+const Content = ({content}) => 
+  content.map(paragraph => <p>{paragraph}</p>)
