71c54feb35aaaebfb9fe3f2b5d928d6d571603ad
Alvin Zhao <14051545+yzAlvin@users.noreply.github.com>
2023-12-15 01:28:50 +1100

    list titles in left pane

--- two-pane-list/src/TwoPaneList.jsx
+++ two-pane-list/src/TwoPaneList.jsx
@@ -1,3 +1,9 @@
 export const TwoPaneList = ({ data }) => {
-  return null
+  const titles = data.map((d) => d.title)
+  return <div className="tile is-ancestor">
+    <div className="tile is-vertical has-background-primary rounded">
+      {titles.map(title => <p>{title}</p>)}
+    </div>
+    <div className="tile is-vertical has-background-info"></div>
+  </div>
 }
