4907ac68ffc0ebe50e0b8e17c7a8c597d6a97491
Alvin Zhao <14051545+yzAlvin@users.noreply.github.com>
2023-12-16 14:10:31 +1100

    ItemFeatureList

--- item-list-with-features/src/ItemList.jsx
+++ item-list-with-features/src/ItemList.jsx
@@ -1,3 +1,38 @@
+import { useState } from "react"
+import AddItemForm from "./AddItemForm"
+import Item from "./Item"
+
+function saveItemsLocally(items) {
+  if (items.length > 0) {
+    localStorage.setItem("items", items)
+  } else {
+    localStorage.removeItem("items")
+  }
+}
+
 export const ItemList = ({ data }) => {
-  return null
+  const [items, setItems] = useState(data)
+  saveItemsLocally(items)
+
+  const handleAddItem = (item) => setItems([...items, item])
+  const removeItem = (itemToRemove) =>
+    setItems(items.filter((item) => item !== itemToRemove))
+  const editItem = (originalItem, editedItem) =>
+    setItems(items.map((item) => (item === originalItem ? editedItem : item)))
+
+  return (
+    <>
+      <div className="box">
+        {items?.map((item) => (
+          <Item
+            key={item}
+            item={item}
+            removeItem={removeItem}
+            editItem={editItem}
+          />
+        ))}
+        <AddItemForm handleAddItems={handleAddItem} />
+      </div>
+    </>
+  )
 }
